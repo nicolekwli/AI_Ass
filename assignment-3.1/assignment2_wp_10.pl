d9b47076aacb1ae9a19d540409716178db1b975a
Lucas Dale <ld17852@bristol.ac.uk>
2020-04-06 12:00:46 +0100

    Part two done!! :D

--- assignment-3.1/assignment2_wp_10.pl
+++ assignment-3.1/assignment2_wp_10.pl
@@ -8,7 +8,27 @@ find_identity(A):-
   ).
 
 find_identity_2(A):-
-  A='Not yet implemented'.
+  % initialises the list of all possible actors and the links on their pages
+  findall((Actor, Links), (actor(Actor), wp(Actor, Text), findall(Link, wt_link(Text, Link), Links)), Bag),
+  % recursively eliminate actors
+  find_from_link(Bag, A).
+
+% custom member check predicate to work with lists of tuples
+memberCustom(X, (_A,[X|_])).
+memberCustom(X, (A,[_|Xs])) :- memberCustom(X, (A,Xs)).
+
+% predicate to eliminate actors without link L from the list
+remove_actors(Bag, L, X):-
+  include(memberCustom(L), Bag, Result),
+  X=Result.
+
+% recursive predicate to find actors by the links on their page
+find_from_link(Bag, A):-
+  % if Bag is length 1, then you've found the actor
+  (length(Bag,1) -> [(X,_)] = Bag, A = X
+  %otherwise, get a new link and remove from there
+  ; otherwise -> agent_ask_oracle(oscar,o(1),link,L), remove_actors(Bag, L, Bag2), find_from_link(Bag2, A)
+  ).
 
 find_identity_o(A):-
   A='Not yet implemented'.
