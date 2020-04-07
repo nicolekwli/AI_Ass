3fd4966b1807cf8d4546c0b785a5e474dc7a2a8d
nicolekwli <nicole.kwli.0100@gmail.com>
2020-04-08 00:45:19 +0100

    added comments, git status are todo/ ideas

---
+++ assignment-3.1/assignment1_12345.pl
@@ -0,0 +1,4 @@
+candidate_number(12345).
+
+>?-q1(ailp_start_position(P).).
+>?-q2a(new_pos(p(1,1), e, P).).
--- assignment-3.1/assignment2_10.pl
+++ assignment-3.1/assignment2_10.pl
@@ -1,11 +1,24 @@
 candidate_number(10).
 
 % finds (some poor) solutions using a (naive) depth-first search algorithm
+% finds the minimal cost of solving any task of the form (go(p(x,y))), find(o(n),c(n))
+% find path using energy available (part 1)
+% find path considering more stuff (part3/4)
+
+% for example solve_task(+go(Pos), -Cost).
+% fail when task not feasible/ no energy
 solve_task(Task,Cost):-
+  %openList = [],
+  %closedList = [P],
   my_agent(Agent),
+
+  % I assume this gets P returned as the current position
   query_world( agent_current_position, [Agent,P] ),
+  
   solve_task_bt(Task,[c(0,P),P],0,R,Cost,_NewPos),!,  % prune choice point for efficiency
   reverse(R,[_Init|Path]),
+
+  % This performs the path found 
   query_world( agent_do_moves, [Agent,Path] ).
 
 %%%%%%%%%% Useful predicates %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -16,14 +29,41 @@ solve_task(Task,Cost):-
 %       from the start position (at the end) to the current position P (at the head)
 %    F is the cost of getting from the start position to P
 % Depth is the current search depth
+
 solve_task_bt(Task,Current,Depth,RPath,[cost(Cost),depth(Depth)],NewPos) :-
   achieved(Task,Current,RPath,Cost,NewPos).
+
+
+% I assume we edit this
+% change this from BFS to A*
+% Will need to calculate manhatten distance using map_distance(+Pos1,+Pos2,-Dist)
 solve_task_bt(Task,Current,D,RR,Cost,NewPos) :-
+  % Why make it a current thing but its not used -- omg is it to split it up into 
+    % variables inside the function that we can use within the function
   Current = [c(F,P)|RPath],
+
+  % gives adjacent locations
+    % (cur_pos, new_posS, new_posS, 1? )
+  % !!might want to create a new search function that gives a list of new positions instead of 1
+    % !!then calculate new cost for all of them
+      % !!then get the smallest one
+        % !!them recurse
   search(P,P1,R,C),
+
+  % is the new positions in the path we've been
   \+ memberchk(R,RPath),  % check we have not been here already
+  
+  % Otherwise continue so we increase depth
   D1 is D+1,
-  F1 is F+C,
+
+  % and also cost coz we moving one more step
+    % !!new cost = (cost so far) F + (heuristic: manhatten distance from current to final distance) H 
+  map_distance(P, NewPos, H),
+  F1 is F + H,
+  %F1 is F+C,
+
+  % and we find another move with low cost
+    % oh wait old one doesn't do that new one needs to
   solve_task_bt(Task,[c(F1,P1),R|RPath],D1,RR,Cost,NewPos).  % backtrack search
 
 % achieved - detects when the specified task has been solved
@@ -32,12 +72,14 @@ achieved(go(Exit),Current,RPath,Cost,NewPos) :-
   ( Exit=none -> true
   ; otherwise -> RPath = [Exit|_]
   ).
+
 achieved(find(O),Current,RPath,Cost,NewPos) :-
   Current = [c(Cost,NewPos)|RPath],
   ( O=none    -> true
   ; otherwise -> RPath = [Last|_],map_adjacent(Last,_,O)
   ).
 
+% (cur_pos_so_far, p1, R, 1)
 search(F,N,N,1) :-
   map_adjacent(F,N,empty).
 
