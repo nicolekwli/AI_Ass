d38245622d7626556848e3e5cfab3086ada944a6
Sharlene <sharlenedsilva@gmail.com>
2020-05-02 16:44:33 +0100

    only find oracle works

--- assignment-3.1/assignment2_10.pl
+++ assignment-3.1/assignment2_10.pl
@@ -23,46 +23,34 @@ solve_task(Task,Cost):-
 
 %%%%%%%%%% Useful predicates %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % solve_task_bt(+Task,+Current,+Depth,-RPath,-Cost,-NewPos)
-% Current is of the form [c(F,P)|RPath]; 
-%    RPath is a list of positions denoting the current path (in reverse) 
-%       from the start position (at the end) to the current position P (at the head)
-%    F is the cost of getting from the start position to P
-% Depth is the current search depth
-
 solve_task_bt(Task,Current, ClosedList, Depth,RPath,[cost(Cost),depth(Depth)],NewPos) :-
   achieved(Task,Current,RPath,Cost,NewPos).
 
 
 % A* algorithm
 solve_task_bt(Task,Current, ClosedList, D,RR,Cost,NewPos) :-
-
   Current = [c(F, G, P) | RPath],
   RPath = [RPath_h| RPath_t],
-
-  % IF IT IS A GO -----!! This needs changing so it accomodates find() as well !!----- 
   Task = go(Final),
-
   ClosedList = [p(Pos_cx, Pos_cy) | Pos_ct],
 
-  
-
-  write('\nDEPTH'),
-  write(P),
+  write('\n CLOSED LIST'),
+  write($ClosedList),
 
   % Gets a list of possible next positions using setof involving search
   % setof(+Template, +Goal, -Set) 
     % binds Set to the list of all instances of Template satisfying the goal Goal
   setof(c(NextPos, Next_H), search(P, Final, NextPos, Next_H, 1), PossChildren), 
-  write($PossChildren),
+  % write($PossChildren),
 
   write('GOING INTO GETTING NEXT BEST MOVE FROM THE LIST'),
 
-  write('\nCLSOED LIST'),
-  write($ClosedList),
+  % write('\nCLSOED LIST'),
+  % write($ClosedList),
 
   % if this breaks we need to backtrack to the move that led to the deadend
     % use condition
-    
+  % TODO: make 99 a larger number
   get_best_next_move(PossChildren, RPath, Final, ClosedList, 99, Move),
   write('DONE'),
   write($Move),
@@ -71,30 +59,81 @@ solve_task_bt(Task,Current, ClosedList, D,RR,Cost,NewPos) :-
   G1 is G + 1,
   
   get_head(Move, NewMove),
-  write($NewMove),
   NewMove = c(MovePos, MoveG),
-  write($NewMove),
  
   ( MoveG == 99 -> write('NEED TO BACKTRACK TO LAST POS'), write($MovePos), 
-                                                            RPath_t = [RPath_th | RPath_tt],
-                                                            map_distance(RPath_h, Final, H1), 
-                                                            F1 is G1 + H1, 
-                                                            write('F1'),
-                                                            write($F1),
-                                                            NextMovePos = RPath_th, 
-                                                            write($NextMovePos),
-                                                            
-                                                            RPathNew = RPath_tt,
-                                                            write('\nDONEDONEDONE\n'),
-                                                            ClosedListNew = [P|ClosedList]
+    RPath_t = [RPath_th | RPath_tt],
+    map_distance(RPath_h, Final, H1), 
+    F1 is G1 + H1, 
+    write('F1'),
+    write($F1),
+    NextMovePos = RPath_th, 
+    write($NextMovePos),
+    
+    RPathNew = RPath_tt,
+    write('\nDONEDONEDONE\n'),
+    ClosedListNew = [P|ClosedList]
                                                                              
     ;map_distance(MovePos, Final, H1), F1 is G1 + H1, NextMovePos = MovePos, RPathNew = RPath, ClosedListNew = ClosedList
   ),
   
-  
-  
   % and we have found the next position for the agent to move to
   solve_task_bt(Task,[c(F1, G1, NextMovePos), NextMovePos | RPathNew], ClosedListNew, D1,RR,Cost,NewPos).  % backtrack search
+
+
+%%%%%%%%%%%%%%%%%%%%
+%%% FOR FIND!!
+solve_task_bt(Task,Current, ClosedList, D,RR,Cost,NewPos) :-
+  write('\nin find'),
+  Current = [c(F, G, P) | RPath],
+  RPath = [RPath_h| RPath_t],
+  Task = find(Obj),
+  write($Obj),
+  ClosedList = [p(Pos_cx, Pos_cy) | Pos_ct],
+
+  % Gets a list of possible next positions using setof involving search
+  % setof(+Template, +Goal, -Set) 
+    % binds Set to the list of all instances of Template satisfying the goal Goal
+  setof(c(NextPos, 0), map_adjacent(P, NextPos, empty), PossChildren),
+  write('\n setof done'),
+
+  % if this breaks we need to backtrack to the move that led to the deadend
+    % use condition
+  % TODO: make 99 a larger number
+  get_best_next_move(PossChildren, RPath, Obj, ClosedList, 99, Move),
+  write('\n next best move done'),
+
+  % depths
+  D1 is D+1,
+  G1 is G+1,
+  
+  get_head(Move, NewMove),
+  NewMove = c(MovePos, MoveG),
+ 
+  ( MoveG == 99 -> write('NEED TO BACKTRACK TO LAST POS'), 
+    write($MovePos), 
+    RPath_t = [RPath_th | RPath_tt],
+    map_distance(RPath_h, RPath_h, H1), 
+    write('\nH1'),
+    write($H1),
+    F1 is G1 + H1, 
+    write('F1'),
+    write($F1),
+    NextMovePos = RPath_th, 
+    % write($NextMovePos),
+    
+    RPathNew = RPath_tt,
+    write('\nDONEDONEDONE\n'),
+    ClosedListNew = [P|ClosedList]
+                                                                             
+    ;map_distance(MovePos, MovePos, H1), F1 is G1 + H1, NextMovePos = MovePos, RPathNew = RPath, ClosedListNew = ClosedList
+  ),
+
+  
+  % and we have found the next position for the agent to move to
+  solve_task_bt(Task,[c(F1, G1, NextMovePos), NextMovePos | RPathNew], ClosedListNew, D1, RR, Cost, NewPos).  % backtrack search
+
+%%%%%%%%%%%%%%%%%%%%%%
 
 
 %% achieved - detects when the specified task has been solved
@@ -118,13 +157,16 @@ search(F, Final, N, H, 1) :-
   map_adjacent(F,N,empty),
   map_distance(N, Final, H).
 
+% search(F, N, N, 1) :-
+%   map_adjacent(F,N,empty).
+
 
 % This is the main bit that gets the best aka lowest cost next move 
 get_best_next_move([], RPath, Final, ClosedList, BestH, Move).
 
 
 get_best_next_move([c(Pos, Pos_h) | []], RPath, Final, [], BestH, Move) :-
-  write('rpathA\n'),
+  write('\nrpathA '),
   write($RPath),
   write('current1'),
   write($Pos),
@@ -134,21 +176,21 @@ get_best_next_move([c(Pos, Pos_h) | []], RPath, Final, [], BestH, Move) :-
       ;( \+check_if_dead_end(Pos) -> write('This is a dead end.'), Move = [c(p(0,0), 99)]
         ;( Pos == Final -> write('Goal found'), Move = [c(Pos, 0)]
             ;( Pos_h < BestH -> write('G is less than1'), Move = [c(Pos, Pos_h)]
-            )
+          )
         )
       )
     )
   ).
 
 get_best_next_move([c(Pos, Pos_h) | []], RPath, Final, ClosedList, BestH, Move) :-
-  write('rpathA\n'),
+  write('\nrpathA '),
   write($RPath),
   %\+memberchk(Pos, RPath),
   %write('not in path\n'),
   write('current1'),
   write($Pos),
 
-  write('\nCLSOED LIST1'),
+  write('\nCLOSED LIST1'),
   write($ClosedList),
 
   (\+check_if_in_path(Pos, ClosedList) -> write('IS IN CLOSED LIST'), Move = [c(Pos, 99)]
@@ -164,7 +206,7 @@ get_best_next_move([c(Pos, Pos_h) | []], RPath, Final, ClosedList, BestH, Move) 
 
 % get_best_next_move([c(Pos, Pos_g)|Children], RPath, Final, BestG, Move) :-
 get_best_next_move([c(Pos, Pos_h)|Children], RPath, Final, ClosedList, BestH, Move) :-
-  write('rpathB\n'),
+  write('\nrpathB '),
   write($RPath),
   %PossChildren = [c(Pos, Pos_g)|Children],
   get_best_next_move(Children, RPath, Final, ClosedList, BestH, Move_t),
@@ -180,7 +222,7 @@ get_best_next_move([c(Pos, Pos_h)|Children], RPath, Final, ClosedList, BestH, Mo
   write($Pos_t),
   write($Pos_th),
 
-  write('\nCLSOED LIST2'),
+  write('\nCLOSED LIST2'),
   write($ClosedList),
 
   % \not +provable,
@@ -204,12 +246,14 @@ get_best_next_move([c(Pos, Pos_h)|Children], RPath, Final, ClosedList, BestH, Mo
     )
   ).
 
+
 get_head([X], X).
 get_head([X|_], X).
 
 
 check_if_in_path(Pos, RPath) :-
   \+memberchk(Pos, RPath).
+
 % should maybe include oracles and shits
 check_if_dead_end(P) :-
   setof(Child, map_adjacent(P, Child, empty), Poss).
