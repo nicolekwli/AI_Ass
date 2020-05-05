73fcaa09416cefee55b6bc2d6d60d44e0578293b
nicolekwli <nicolekwli19@gmail.com>
2020-05-05 11:43:38 +0100

    works but not entirely bfs

--- assignment-3.1/assignment2_10.pl
+++ assignment-3.1/assignment2_10.pl
@@ -10,92 +10,101 @@ candidate_number(10).
 solve_task(Task,Cost):-
   my_agent(Agent),
   query_world( agent_current_position, [Agent,P] ),
+  query_world(agent_current_energy, [Agent, Energy]),
 
   ClosedList = [p(1,1)],
   
   % proceed to find the shortest path
-  solve_task_bt(Task,[c(0,0,P),P], ClosedList, 0,R,Cost,_NewPos),!,
+  solve_task_bt(Energy, Task,[c(0,0,P),P], ClosedList, 0,R,Cost,_NewPos),!,
   reverse(R,[_Init|Path]),
 
   % This performs the path found 
   query_world( agent_do_moves, [Agent,Path] ).
 
-
-%%%%%%%%%% Useful predicates %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% solve_task_bt(+Task,+Current,+Depth,-RPath,-Cost,-NewPos)
-% Current is of the form [c(F,P)|RPath]; 
-%    RPath is a list of positions denoting the current path (in reverse) 
-%       from the start position (at the end) to the current position P (at the head)
-%    F is the cost of getting from the start position to P
-% Depth is the current search depth
-
-solve_task_bt(Task,Current, ClosedList, Depth,RPath,[cost(Cost),depth(Depth)],NewPos) :-
+solve_task_bt(Energy, Task,Current, ClosedList, Depth,RPath,[cost(Cost),depth(Depth)],NewPos) :-
   achieved(Task,Current,RPath,Cost,NewPos).
 
 
 % A* algorithm
-solve_task_bt(Task,Current, ClosedList, D,RR,Cost,NewPos) :-
-
-  Current = [c(F, G, P) | RPath],
-  RPath = [RPath_h| RPath_t],
+solve_task_bt(Energy, Task,Current, ClosedList, D,RR,Cost,NewPos) :-
 
   % IF IT IS A GO -----!! This needs changing so it accomodates find() as well !!----- 
   Task = go(Final),
+  Current = [c(F, G, P) | RPath],
+  RPath = [RPath_h| RPath_t],
 
   ClosedList = [p(Pos_cx, Pos_cy) | Pos_ct],
-
-  
-
-  write('\nDEPTH'),
-  write(P),
 
   % Gets a list of possible next positions using setof involving search
   % setof(+Template, +Goal, -Set) 
     % binds Set to the list of all instances of Template satisfying the goal Goal
-  setof(c(NextPos, Next_H), search(P, Final, NextPos, Next_H, 1), PossChildren), 
+  setof(c(NextPos, Next_H), search(P, Task, NextPos, D, Next_H, 1), PossChildren), 
   write($PossChildren),
-
-  write('GOING INTO GETTING NEXT BEST MOVE FROM THE LIST'),
-
-  write('\nCLSOED LIST'),
-  write($ClosedList),
-
-  % if this breaks we need to backtrack to the move that led to the deadend
-    % use condition
     
   get_best_next_move(PossChildren, RPath, Final, ClosedList, 99, Move),
-  write('DONE'),
-  write($Move),
   
   D1 is D+1,
   G1 is G + 1,
   
   get_head(Move, NewMove),
-  write($NewMove),
   NewMove = c(MovePos, MoveG),
   write($NewMove),
  
-  ( MoveG == 99 -> write('NEED TO BACKTRACK TO LAST POS'), write($MovePos), 
+  ( MoveG == 999 -> write('NEED TO BACKTRACK TO LAST POS'), write($MovePos), 
                                                             RPath_t = [RPath_th | RPath_tt],
-                                                            map_distance(RPath_h, Final, H1), 
+                                                            % create new predicate taking in task, return 0 if find
+                                                            calc_dist(RPath_h, Task, H1),
+                                                            % map_distance(RPath_h, Final, H1), 
                                                             F1 is G1 + H1, 
-                                                            write('F1'),
-                                                            write($F1),
                                                             NextMovePos = RPath_th, 
                                                             write($NextMovePos),
                                                             
                                                             RPathNew = RPath_tt,
-                                                            write('\nDONEDONEDONE\n'),
+
                                                             ClosedListNew = [P|ClosedList]
                                                                              
-    ;map_distance(MovePos, Final, H1), F1 is G1 + H1, NextMovePos = MovePos, RPathNew = RPath, ClosedListNew = ClosedList
+    ;calc_dist(MovePos, Task, H1), F1 is G1 + H1, NextMovePos = MovePos, RPathNew = RPath, ClosedListNew = ClosedList
+  ),
+  % and we have found the next position for the agent to move to
+  solve_task_bt(Energy, Task,[c(F1, G1, NextMovePos), NextMovePos | RPathNew], ClosedListNew, D1,RR,Cost,NewPos).
+
+
+% this does depth
+solve_task_bt(Energy, Task,Current, ClosedList, D,RR,Cost,NewPos) :-
+  Task = find(Final),
+  Current = [c(F, G, P) | RPath],
+  RPath = [RPath_h| RPath_t],
+  write('\nPATH'),
+  write(RPath),
+  
+  ClosedList = [p(Pos_cx, Pos_cy) | Pos_ct],
+
+  setof(c(NextPos, Next_H, NextObj), search(P, Task, NextPos, D,Next_H, NextObj, 1), PossChildren), 
+
+  write('\nPOsSCHILD'),
+  write(PossChildren),
+
+  get_best_next_move(PossChildren, RPath, Final, ClosedList, 999, Move),
+  
+  get_head(Move, NewMove),
+  NewMove = c(MovePos, MoveG),
+  ( Energy < MoveG -> Backtrack is 999
+    ; write('no need to backtrack'), Backtrack is MoveG
   ),
   
-  
-  
-  % and we have found the next position for the agent to move to
-  solve_task_bt(Task,[c(F1, G1, NextMovePos), NextMovePos | RPathNew], ClosedListNew, D1,RR,Cost,NewPos).  % backtrack search
+  ( Backtrack == 999 -> write('NEED TO BACKTRACK TO LAST POS'), G1 is G-1, 
+                                                            RPath_t = [RPath_th | RPath_tt],
+                                                            calc_dist(RPath_h, Task, H1),
+                                                            F1 is G1 + H1, 
+                                                            NextMovePos = RPath_th, 
+                                                            RPathNew = RPath_tt,
+                                                            ClosedListNew = [P|ClosedList],
+                                                            D1 is D-1
 
+                                                                            
+    ;calc_dist(MovePos, Task, H1), G1 is G+1,F1 is G1 + H1, NextMovePos = MovePos, RPathNew = RPath, ClosedListNew = ClosedList, D1 is D+1
+  ),
+  solve_task_bt(Energy, Task,[c(F1, G1, NextMovePos), NextMovePos | RPathNew], ClosedListNew, D1,RR,Cost,NewPos).
 
 %% achieved - detects when the specified task has been solved
 achieved(go(Exit),Current,RPath,Cost,NewPos) :-
@@ -114,58 +123,87 @@ achieved(find(O),Current,RPath,Cost,NewPos) :-
 
 
 % this is called by setof
-search(F, Final, N, H, 1) :-
+search(F, Task, N, D, H, 1) :-
+  Task = go(Final),
   map_adjacent(F,N,empty),
   map_distance(N, Final, H).
+
+search(F, Task, N, D, D, NextObj, 1) :-
+  Task = find(Final),
+  map_adjacent(F,N,NextObj),
+  write(N).
+
+search(P, Task, Pos, NextObj) :-
+  Task = find(Final),
+  map_adjacent(P, Pos, NextObj).
+
+calc_dist(P, Task, H) :-
+  Task = go(Final),
+  map_distance(P, Final, H).
+
+calc_dist(P, Task, 0) :-
+  Task = find(Final).
 
 
 % This is the main bit that gets the best aka lowest cost next move 
 get_best_next_move([], RPath, Final, ClosedList, BestH, Move).
 
 
-get_best_next_move([c(Pos, Pos_h) | []], RPath, Final, [], BestH, Move) :-
-  write('rpathA\n'),
-  write($RPath),
-  write('current1'),
+get_best_next_move([c(Pos, Pos_h, Object) | []], RPath, Final, [], BestH, Move) :-
+  %write('rpathA\n'),
+  %write($RPath),
+  write('current!'),
   write($Pos),
   
-  (\+check_if_in_path(Pos, ClosedList) -> write('IS IN CLOSED LIST'), Move = [c(Pos, 99)]
-    ;( \+check_if_in_path(Pos, RPath) -> write('IT IS IN PATH'), Move = [c(Pos, 99)]
-      ;( \+check_if_dead_end(Pos) -> write('This is a dead end.'), Move = [c(p(0,0), 99)]
-        ;( Pos == Final -> write('Goal found'), Move = [c(Pos, 0)]
-            ;( Pos_h < BestH -> write('G is less than1'), Move = [c(Pos, Pos_h)]
+  ( Object == empty ->
+          (\+check_if_in_path(Pos, ClosedList) -> write('IS IN CLOSED LIST'), Move = [c(Pos, 999)]
+            ;( \+check_if_in_path(Pos, RPath) -> write('IT IS IN PATH'), Move = [c(Pos, 999)]
+              ;( \+check_if_dead_end(Pos) -> write('This is a dead end.'), Move = [c(p(0,0), 999)]
+                ;( Pos == Final -> write('Goal found'), Move = [c(Pos, 0)]
+                  ;( Object == Final -> write('OBJECT FOUND'), Move = [c(Pos, 0)]
+                    ;( Pos_h < BestH -> write('G is less than1'), Move = [c(Pos, Pos_h)]
+                    )
+                  )
+                )  
+              )
             )
-        )
-      )
+          )
+    ; (Object == Final -> write('OBJECT FOUND'), Move = [c(Pos, 0)]
+      ;Move = [c(Pos, 999)]
     )
   ).
 
-get_best_next_move([c(Pos, Pos_h) | []], RPath, Final, ClosedList, BestH, Move) :-
-  write('rpathA\n'),
-  write($RPath),
-  %\+memberchk(Pos, RPath),
-  %write('not in path\n'),
+get_best_next_move([c(Pos, Pos_h, Object) | []], RPath, Final, ClosedList, BestH, Move) :-
+  %write('rpathA\n'),
+  %write($RPath),
+
   write('current1'),
   write($Pos),
 
-  write('\nCLSOED LIST1'),
-  write($ClosedList),
-
-  (\+check_if_in_path(Pos, ClosedList) -> write('IS IN CLOSED LIST'), Move = [c(Pos, 99)]
-    ;( \+check_if_in_path(Pos, RPath) -> write('IT IS IN PATH'), Move = [c(Pos, 99)] 
-      ;( \+check_if_dead_end(Pos) -> write('This is a dead end.'), Move = [c(p(0,0), 99)]
-        ;( Pos == Final -> write('Goal found'), Move = [c(Pos, 0)]
-            ;( Pos_h < BestH -> write('G is less than1'), Move = [c(Pos, Pos_h)]
+  %write('\nCLSOED LIST1'),
+  %write($ClosedList),
+  ( Object == empty ->
+    (\+check_if_in_path(Pos, ClosedList) -> write('IS IN CLOSED LIST'), Move = [c(Pos, 999)]
+      ;( \+check_if_in_path(Pos, RPath) -> write('IT IS IN PATH'), Move = [c(Pos, 999)] 
+        ;( \+check_if_dead_end(Pos) -> write('This is a dead end.'), Move = [c(p(0,0), 999)]
+          ;( Pos == Final -> write('Goal found'), Move = [c(Pos, 0)]
+            ;( Object == Final -> write('OBJECT FOUND'), Move = [c(Pos, 0)]
+              ;( Pos_h < BestH -> write('G is less than1'), Move = [c(Pos, Pos_h)]
+              )
             )
+          )
         )
       )
     )
-  ).
+  ; (Object == Final -> write('OBJECT FOUND'), Move = [c(Pos, 0)]
+    ;Move = [c(Pos, 999)]
+  )
+).
 
 % get_best_next_move([c(Pos, Pos_g)|Children], RPath, Final, BestG, Move) :-
-get_best_next_move([c(Pos, Pos_h)|Children], RPath, Final, ClosedList, BestH, Move) :-
-  write('rpathB\n'),
-  write($RPath),
+get_best_next_move([c(Pos, Pos_h, Object)|Children], RPath, Final, ClosedList, BestH, Move) :-
+  %write('rpathB\n'),
+  %write($RPath),
   %PossChildren = [c(Pos, Pos_g)|Children],
   get_best_next_move(Children, RPath, Final, ClosedList, BestH, Move_t),
 
@@ -180,29 +218,32 @@ get_best_next_move([c(Pos, Pos_h)|Children], RPath, Final, ClosedList, BestH, Mo
   write($Pos_t),
   write($Pos_th),
 
-  write('\nCLSOED LIST2'),
-  write($ClosedList),
-
-  % \not +provable,
-  % \+memberchk(Child, RPath),
-  % write('its not in path'),
+  %write('\nCLSOED LIST2'),
+  %write($ClosedList),
 
   % Check if it is the end
       % chek if child has more childs -> if not ignore
       % check if child is in the closed list(lets say RPath)  -> if is, ignore
       % check if child is in the open list -> if not then add
         % if it is, check g to destination, if bterr change ,value in open list
-  (\+check_if_in_path(Pos, ClosedList) -> write('IS IN CLOSED LIST'), Move = Move_t
-    ;( \+check_if_in_path(Pos, RPath) -> write('IT IS IN PATH'), Move = Move_t
-      ;( \+check_if_dead_end(Pos) -> write('This is a dead end.'), Move = [Move_t]
-        ;( Pos == Final -> write('Goal found'), Move = [c(Pos, 0)]
-          ;( Pos_h < Pos_th -> write('G is less than2'), Move = [Child|Move_t]
-            ; write('THIS IS THE POS IT BREAK'), write(Pos), Move = Move_t
+  ( Object == empty ->
+    (\+check_if_in_path(Pos, ClosedList) -> write('IS IN CLOSED LIST'), Move = Move_t
+      ;( \+check_if_in_path(Pos, RPath) -> write('IT IS IN PATH'), Move = Move_t
+        ;( \+check_if_dead_end(Pos) -> write('This is a dead end.'), Move = [Move_t]
+          ;( Pos == Final -> write('Goal found'), Move = [c(Pos, 0)]
+            ;( Object == Final -> write('OBJECT FOUND'), Move = [c(Pos, 0)]
+              ;( Pos_h < Pos_th -> write('G is less than2'), Move = [Child|Move_t]
+                ; write('THIS IS THE POS IT BREAK'), write(Pos), Move = Move_t
+              )
+            )
           )
         )
       )
     )
-  ).
+  ; (Object == Final -> write('OBJECT FOUND'), Move = [c(Pos, 0)]
+    ;Move = Move_t
+  )
+).
 
 get_head([X], X).
 get_head([X|_], X).
@@ -213,3 +254,4 @@ check_if_in_path(Pos, RPath) :-
 % should maybe include oracles and shits
 check_if_dead_end(P) :-
   setof(Child, map_adjacent(P, Child, empty), Poss).
+  
